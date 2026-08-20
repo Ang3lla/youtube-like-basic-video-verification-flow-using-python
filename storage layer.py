a51ca12cd704f2ee4bde018e5e82751a09cd27ed
Angella <angellahirwa7@email.com>
2026-08-20 21:51:11 +0200

    added a a storage layer to this

---
+++ storage layer.py
@@ -0,0 +1,15 @@
+videos = ["python basics", "backend basics", "cat memes"]
+videos.append("dog memes")
+
+users = [
+    {"id": 1, "name": "Joe"},
+    {"id": 2, "name": "Johnna"},
+    {"id": 3, "name": "Johnnathan pilli"},
+]
+for user in users:
+    print(user["name"])
+
+
+video = {"id": 34, "name": "the jumping fox"}
+
+cordinates_location = (12.4, 14.6, 13.7)
